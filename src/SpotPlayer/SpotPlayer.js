baf046c07043930105b114a1e9b0fffaccb6f58c
neal <winzipsdo@outlook.com>
2018-04-06 19:03:34 +0800

    new component SpotPlayer

---
+++ src/SpotPlayer/SpotPlayer.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import './SpotPlayer.css';
+
+export default class SpotPlayer extends Component {
+    constructor(props) {
+        super(props);
+        const autoPlay = this.props.autoPlay ? this.props.autoPlay : false;
+        this.state = {
+            isPlay: autoPlay,
+            autoPlay: autoPlay,
+            loop: true
+        };
+        this.handleToggle = this.handleToggle.bind(this);
+        this.audioPlay = this.audioPlay.bind(this);
+        this.audioPause = this.audioPause.bind(this);
+        this.handleDoubleClick = this.handleDoubleClick.bind(this);
+    }
+
+    componentDidMount() {
+        if (this.props.autoPlay){
+            this.audioPlay();
+        }
+    }
+
+    audioPause() {
+        let audio = this.refs.audio;
+        let outerContainer = this.refs.outerContainer;
+        let outerTransform = getComputedStyle(outerContainer).transform;
+        let innerContainer = this.refs.innerContainer;
+        let innerTransform = getComputedStyle(innerContainer).transform;
+
+        audio.pause();
+        innerContainer.classList.remove('isPlay');
+        outerContainer.style.transform = outerTransform === 'none'
+            ? innerTransform
+            : innerTransform.concat(outerTransform);
+    }
+
+    audioPlay() {
+        this.refs.audio.play();
+        this.refs.innerContainer.classList.add('isPlay');
+    }
+
+    handleDoubleClick(){
+        let audio = this.refs.audio;
+        audio.currentTime = 0;
+        this.audioPlay();
+    }
+
+    handleToggle() {
+        if (this.state.isPlay) {
+            this.audioPause()
+        } else {
+            this.audioPlay()
+        }
+
+        this.setState({
+            isPlay: !this.state.isPlay
+        });
+    }
+
+    render() {
+        return (
+            <div ref='outerContainer'
+                 className='outerContainer'
+                 onClick={this.handleToggle}
+                 onDoubleClick={this.handleDoubleClick}>
+                <div ref='innerContainer' className='innerContainer'>
+                    <audio ref="audio"
+                           loop={this.state.loop}>
+                        <source src={this.props.music || null}/>
+                        您的浏览器版本过低。
+                    </audio>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SpotPlayer from './SpotPlayer/SpotPlayer';
+import registerServiceWorker from './registerServiceWorker';
+import music from './media/烟把儿 - 纸短情长（完整版）.mp3';
+
+ReactDOM.render(<SpotPlayer autoPlay={true}
+                            music={music}/>, document.getElementById('player'));
+registerServiceWorker();
