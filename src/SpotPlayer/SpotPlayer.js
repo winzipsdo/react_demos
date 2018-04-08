2b4e833da12546c8134423c92494c1245d0cc481
neal <winzipsdo@outlook.com>
2018-04-09 00:10:42 +0800

    sass test

--- src/SpotPlayer/SpotPlayer.js
+++ src/SpotPlayer/SpotPlayer.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './SpotPlayer.css';
+import './SpotPlayer.scss';
 
 export default class SpotPlayer extends Component {
     constructor(props) {
--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import SpotPlayer from './SpotPlayer/SpotPlayer';
 import registerServiceWorker from './registerServiceWorker';
 import music from './media/烟把儿 - 纸短情长（完整版）.mp3';
 
-ReactDOM.render(<SpotPlayer autoPlay={true}
+ReactDOM.render(<SpotPlayer autoPlay={false}
                             music={music}/>, document.getElementById('player'));
 registerServiceWorker();
